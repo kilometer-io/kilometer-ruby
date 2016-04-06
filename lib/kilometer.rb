dc6701717081ffddaa0f9ccb3dbb4ba765126053
Yuri Kroz <yuri@kilometer.io>
2016-04-06 08:53:44 +0300

    Implemented add_user method

--- lib/kilometer.rb
+++ lib/kilometer.rb
@@ -1,17 +1,20 @@
 require_relative 'kilometer/version'
 require 'date'
+require 'net/http'
+require 'json'
+require 'uri'
 
 module Kilometer
+
   class EventsAPIClient
     # Use Kilometer::EventsAPIClient to track events and manage users properties.
 
-
-    @@HEADER_CUSTOMER_APP_ID = 'Customer-App-Id'
-    @@HEADER_TIMESTAMP = 'Timestamp'
-    @@HEADER_CLIENT_TYPE = 'Client-Type'
-    @@HEADER_CONTENT_TYPE = 'Content-Type'
-    @@CLIENT_TYPE = 'ruby'
-    @@CONTENT_TYPE = 'application/json'
+    HEADER_CUSTOMER_APP_ID = 'Customer-App-Id'
+    HEADER_TIMESTAMP = 'Timestamp'
+    HEADER_CLIENT_TYPE = 'Client-Type'
+    HEADER_CONTENT_TYPE = 'Content-Type'
+    CLIENT_TYPE = 'ruby'
+    CONTENT_TYPE = 'application/json'
 
     def initialize(token, endpoint_url=nil)
       @token = token
@@ -26,9 +29,28 @@ module Kilometer
     #     headers: custom request headers (if isn't set default values are used)
     #     endpoint_url: where to send the request (if isn't set default value is used)
     def add_user(user_id, headers=nil, endpoint_url=nil)
-      endpoint_url = endpoint_url || @endpoint_url
 
-      url = "#{endpoint_url}/users"
+      uri = URI("#{endpoint_url || @endpoint_url}/users")
+
+      Net::HTTP.start(uri.host, uri.port) do |http|
+
+        request = Net::HTTP::Post.new(uri.request_uri)
+
+        # Set headers
+        if headers
+          headers.each { |header, value| request.add_field(header, value) }
+        else
+          self.set_default_headers(request)
+          request.add_field(HEADER_CONTENT_TYPE, CONTENT_TYPE)
+        end
+
+        # Set payload
+        payload = {user_id: user_id}
+        request.body = payload.to_json
+
+        # Execute the request
+        response = http.request(request)
+      end
     end
 
     # Send an identified event. If a user doesn't exist it will create one.
@@ -61,6 +83,16 @@ module Kilometer
 
       url = "#{endpoint_url}/users/#{user_id}/properties"
     end
+
+    # Gets Net::HTTP request instance and modifies it by adding default HTTP headers.
+    #
+    # WARNING: The method modifies the #request object!
+    def set_default_headers(request)
+      request.add_field(HEADER_CUSTOMER_APP_ID, @token)
+      request.add_field(HEADER_CLIENT_TYPE, CLIENT_TYPE)
+      request.add_field(HEADER_TIMESTAMP, Kilometer::now)
+    end
+
   end
 
   # Get current timestamp in milliseconds
